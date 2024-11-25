8cea9e6a85f7a287412486a0078fc6336945ac09
Elijah Mottey <elijahmottey5@gmail.com>
2024-11-24 22:57:48 -0800

    configuration and database

---
+++ src/main/java/LIVTech/authentication/Models/User.java
@@ -0,0 +1,19 @@
+package LIVTech.authentication.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String resetToken;
+}
